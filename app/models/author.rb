e4e270439ff8dbf91c4440f964fa327499e2dd4f
Jakub Jatczak <j.jatczak@binarapps.com>
2018-07-12 14:27:11 +0200

    [add] partial model tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,9 +3,20 @@ class Author < ActiveRecord::Base
   # has_many :posts, through: :author_posts
   has_many :posts
 
+  scope :old, -> { where('age > 30') }
+  scope :young, -> { where('age < 20') }
+
+  before_create :default_age
+
   validates :name, :surname, presence: true
 
   def fullname
     "#{name} #{surname}"
+  end
+
+  private
+
+  def default_age
+    self.age = 25 unless age
   end
 end
