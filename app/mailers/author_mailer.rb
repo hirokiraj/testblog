099daf05668176eeb509b72adea4b2b15d6215d9
Jakub Jatczak <j.jatczak@binarapps.com>
2018-07-23 13:36:39 +0200

    [add] travis config

--- app/mailers/author_mailer.rb
+++ app/mailers/author_mailer.rb
@@ -1,6 +1,7 @@
 class AuthorMailer < ApplicationMailer
   def hello_mail(author, user)
     @author = author
+    @user = user
     mail(to: user.email, subject: "Info about author #{author.fullname}")
   end
 end
